ac0453c93c003fd3df38ae091ae8761f2d758675
eXory2024 <exory@anio.software>
2025-06-26 20:17:05 +0200

    Use @anio-software/pkg.node-fs-path-type.

--- project/src/__DependenciesXXX.as.ts
+++ project/src/__DependenciesXXX.as.ts
@@ -1,5 +1,5 @@
-import {getTypeOfPath} from "@aniojs/node-fs-path-type"
-//>import {getTypeOfPathSync as getTypeOfPath} from "@aniojs/node-fs-path-type"
+import {getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
+//>import {getTypeOfPathSync as getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
 
 export type __EnkoreFunctionDependencies = {
 	getTypeOfPath: typeof getTypeOfPath
