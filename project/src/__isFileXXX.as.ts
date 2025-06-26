f115a3741ef487fc082e6451530497e5bb32a29a
eXory2024 <exory@anio.software>
2025-06-26 20:11:34 +0200

    Partially migrate from fourtune to enkore.

--- project/src/__isFileXXX.as.ts
+++ project/src/__isFileXXX.as.ts
@@ -1,4 +1,7 @@
-import {useContext, type RuntimeWrappedContextInstance} from "@fourtune/realm-js/runtime"
+import {
+	type EnkoreJSRuntimeContextOptions,
+	createContext
+} from "@anio-software/enkore.js-runtime"
 
 import type {AnioJsDependencies} from "#~synthetic/async.sync/Dependencies.d.mts"
 //>import type {AnioJsDependencies} from "#~synthetic/async.sync/DependenciesSync.d.mts"
@@ -7,12 +10,12 @@ export type {AnioJsDependencies}
 
 export async function implementation(
 //>export function implementation(
-	wrapped_context: RuntimeWrappedContextInstance,
+	contextOptions: EnkoreJSRuntimeContextOptions,
 	dependencies: AnioJsDependencies,
 	input_path: string
 ) : Promise<boolean> {
 //>) : boolean {
-	const context = useContext(wrapped_context, 0)
+	const context = createContext(contextOptions, 0)
 
 	const path_type = await dependencies.getTypeOfPath(input_path)
 //>	const path_type = dependencies.getTypeOfPath(input_path)
