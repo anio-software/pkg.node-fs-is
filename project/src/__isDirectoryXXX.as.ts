03f3f583be542c9a574d39a1b21f508b0a4ac304
eXory2024 <exory@anio.software>
2025-06-26 20:19:32 +0200

    Migrate from camel_case to camelCase.

--- project/src/__isDirectoryXXX.as.ts
+++ project/src/__isDirectoryXXX.as.ts
@@ -12,16 +12,16 @@ export async function __implementation(
 //>export function __implementationSync(
 	contextOptions: EnkoreJSRuntimeContextOptions,
 	dependencies: __EnkoreFunctionDependencies,
-	input_path: string
+	inputPath: string
 ) : Promise<boolean> {
 //>) : boolean {
 	const context = createContext(contextOptions, 0)
 
-	const path_type = await dependencies.getTypeOfPath(input_path)
-//>	const path_type = dependencies.getTypeOfPath(input_path)
+	const pathType = await dependencies.getTypeOfPath(inputPath)
+//>	const pathType = dependencies.getTypeOfPath(inputPath)
 
-	if (path_type === "dir:regular") return true
-	if (path_type === "link:dir") return true
+	if (pathType === "dir:regular") return true
+	if (pathType === "link:dir") return true
 
 	return false
 }
