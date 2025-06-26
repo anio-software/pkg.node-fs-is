8ebe605eb760a2ab70ff0dfe395febf80abd4560
eXory2024 <exory@anio.software>
2025-06-26 20:17:18 +0200

    In enkore dependencies must be a type only import.

--- project/src/__DependenciesXXX.as.ts
+++ project/src/__DependenciesXXX.as.ts
@@ -1,5 +1,5 @@
-import {getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
-//>import {getTypeOfPathSync as getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
+import type {getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
+//>import type {getTypeOfPathSync as getTypeOfPath} from "@anio-software/pkg.node-fs-path-type"
 
 export type __EnkoreFunctionDependencies = {
 	getTypeOfPath: typeof getTypeOfPath
